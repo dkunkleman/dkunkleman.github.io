3d801202c2b5f866a82374e171c9dcd01d05fe86
David Kunkleman <davekunkleman@icloud.com>
2026-08-08 17:46:17 -0500

    Publish isolated repeat photo iPhone test

---
+++ property-intelligence-repeat-photo/sw.js
@@ -0,0 +1,21 @@
+"use strict";
+const CACHE_NAME = "property-intelligence-repeat-photo-poc-v1";
+const APP_ASSETS = [
+  "./", "./index.html", "./styles.css", "./photo-point-core.js", "./idb-store.js", "./app.js", "./manifest.webmanifest", "./DEPLOYMENT_IDENTITY.json"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(APP_ASSETS)).then(() => self.skipWaiting()));
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(caches.keys().then((keys) => Promise.all(keys.filter((key) => key.startsWith("property-intelligence-repeat-photo-poc-") && key !== CACHE_NAME).map((key) => caches.delete(key)))).then(() => self.clients.claim()));
+});
+
+self.addEventListener("fetch", (event) => {
+  if (event.request.method !== "GET") return;
+  event.respondWith(caches.match(event.request).then((cached) => cached || fetch(event.request).then((response) => {
+    if (response.ok && new URL(event.request.url).origin === self.location.origin) caches.open(CACHE_NAME).then((cache) => cache.put(event.request, response.clone()));
+    return response;
+  })));
+});
